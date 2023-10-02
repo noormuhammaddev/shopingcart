f170d633ce763e7ebbb14fab603deb6985997a0b
noor.muhammad@systemsltd.com <noor.muhammad@systemsltd.com>
2023-10-02 15:19:01 +0500

    add react js basic app with show product lists and filter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TopBar from './components/TopBar';
+import ProductListings from './pages/ProductListings/ProductListings';
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+      <ProductListings />
+    </div>
+  );
+}
+
+export default App;
